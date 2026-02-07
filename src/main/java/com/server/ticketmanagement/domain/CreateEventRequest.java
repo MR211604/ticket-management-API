9e3f6088f1e0a47d8a9beb9d4bce9d49069445a3
MR211604 <elmastrollxd34@gmail.com>
2026-02-07 16:20:15 -0600

    chore: adding domain classes for requests and responses

---
+++ src/main/java/com/server/ticketmanagement/domain/CreateEventRequest.java
@@ -0,0 +1,28 @@
+package com.server.ticketmanagement.domain;
+
+import com.server.ticketmanagement.domain.entities.EventStatusEnum;
+import com.server.ticketmanagement.domain.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEventRequest {
+
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+    private EventStatusEnum status;
+
+    private List<CreateTicketTypeRequest> ticketTypes = new ArrayList<>();
+
+}
