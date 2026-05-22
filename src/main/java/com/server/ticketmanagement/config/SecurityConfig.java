d62dfaddd1e2e9a45baf0078e56a7ea20f207cb1
MR211604 <elmastrollxd34@gmail.com>
2026-05-21 18:49:04 -0600

    chore: temporarily changing SecurityConfig

--- src/main/java/com/server/ticketmanagement/config/SecurityConfig.java
+++ src/main/java/com/server/ticketmanagement/config/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.server.ticketmanagement.config;
-import com.server.ticketmanagement.config.JwtAuthenticationConverter;
 import com.server.ticketmanagement.filters.UserProvisioningFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,27 +18,13 @@ public class SecurityConfig {
             UserProvisioningFilter userProvisioningFilter,
             JwtAuthenticationConverter jwtAuthenticationConverter
     ) throws Exception {
-        http.authorizeHttpRequests(
-                authorize ->
-                authorize
-                        .requestMatchers(HttpMethod.GET, "/api/v1/published-events/**")
-                        .permitAll()
-                        .requestMatchers("/api/v1/events").hasRole("ORGANIZER")
-                        .requestMatchers("/api/v1/statistics/**").hasRole("ORGANIZER")
-                        .requestMatchers("/api/v1/ticket-validations").hasRole("STAFF")
-                        .anyRequest().authenticated())
-                .csrf(csrf ->
-                        csrf.disable())
-                .sessionManagement(
-                        session ->
-                                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(
-                                jwt ->
-                                        jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)
-                        ))
-                .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
-
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                );
         return http.build();
     }
 }
